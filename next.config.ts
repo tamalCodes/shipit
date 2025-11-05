30e90a8a5f87aded263aec9b30dbd77c7891e891
tamalCodes <tamalcodes@gmail.com>
2025-11-06 03:21:02 +0530

    fix: config changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/manifest.webmanifest",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/manifest+json",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
